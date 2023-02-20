0a245f719de5e8b390072b3e3724b29c8546dcda
Jeremy Singer-Vine <jsvine@gmail.com>
2023-02-20 14:43:28 -0500

    Flip lat/long

--- viewer/src/components/Facility.svelte
+++ viewer/src/components/Facility.svelte
@@ -9,7 +9,7 @@
     <li><b>State:</b> <span><a href="#/state:{item.state}">{item.state || ""}</a></span></li>
     <li><b>City:</b> <span>{item.city || ""}</span></li>
     <li><b>ZIP Code:</b> <span>{item.zip || ""}</span></li>
-    <li><b>Coordinates:</b> <span>{#if item.lng}{item.lng || ""}, {item.lat || ""} (<a href="https://www.openstreetmap.org/?zoom=14&mlat={item.lat}&mlon={item.lng}" target="_blank" rel="noreferrer">OSM</a> | <a href="https://maps.google.com/maps?q=loc:{item.lat},{item.lng}" target="_blank" rel="noreferrer">Google</a>){/if}</span></li>
+    <li><b>Coordinates:</b> <span>{#if item.lng}{item.lat || ""}, {item.lng || ""} (<a href="https://www.openstreetmap.org/?zoom=14&mlat={item.lat}&mlon={item.lng}" target="_blank" rel="noreferrer">OSM</a> | <a href="https://maps.google.com/maps?q=loc:{item.lat},{item.lng}" target="_blank" rel="noreferrer">Google</a>){/if}</span></li>
     <li><b>Company 1:</b> <span>{item.company_1 || ""}</span></li>
     <li><b>Company 2:</b> <span>{item.company_2 || ""}</span></li>
     <li><b>Operator:</b> <span>{item.operator || ""}</span></li>
