d387976d0d63de84725d253e426ce97924924048
Jeremy Singer-Vine <jsvine@gmail.com>
2023-02-20 08:58:36 -0500

    Edit landing page

--- viewer/src/pages/Landing.svelte
+++ viewer/src/pages/Landing.svelte
@@ -9,10 +9,10 @@
   This tool provides several views on the data:
 </p>
 <ul>
-  <li>A listing of <a href="#/list:states">all states</a> represented in the database.</li>
-  <li>For <b>each state</b> (e.g., <a href="#/state:TX">Texas</a>), county-by-county listing of all facilities that have, at any point, filed an RMP submission.</li>
-  <li>For <b>each facility</b> (e.g., <a href="#/facility:100000070987">this facility in Port Neches, Texas</a>, <a href="https://www.texastribune.org/2022/12/20/texas-chemical-plant-explosion-tpc-port-neches/">the site of a 2019 explosion</a>), some top-level information and a links to all the facility's submissions</li>
-  <li>For <b>each submission</b> (e.g., <a href="#/submission:1000068629">#1000068629</a>), raw data from the core database tables, covering information on the submission and facility, processes, process chemicals, prevention programs, accident history, and executive summary. <i>Note: Although this represents a large chunk of the data, it is not exhaustive of all tables and fields in the database, for which you should consult the raw data, available through the documentation linked above.</i></li>
+  <li>A <a href="#/list:states">listing of all states</a> represented in the database.</li>
+  <li>For <b>each state</b> (e.g., <a href="#/state:TX">Texas</a>) — county-by-county listing of all facilities that have, at any point, filed an RMP submission.</li>
+  <li>For <b>each facility</b> (e.g., <a href="#/facility:100000070987">this facility</a>) — overview information and a table linking to all the facility's submissions</li>
+  <li>For <b>each submission</b> (e.g., <a href="#/submission:43946">#43946</a>) — raw data from the core database tables, covering information on the submission and facility, processes, process chemicals, prevention programs, accident history, and executive summary. <i>Note: Although this represents a large chunk of the data, it is not exhaustive of all tables and fields in the database, for which you should consult the raw data, available through the documentation linked above.</i></li>
 </ul>
 <style>
   li + li {
